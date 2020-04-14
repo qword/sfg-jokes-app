f04d0e18271771419edfc98d8abe17db0d4233b6
Gianluigi Disconzi <gianluigi.disconzi@gmail.com>
2020-04-14 15:39:13 +0200

    Add controller

---
+++ src/main/java/qword/spring/jokeapp/controller/JokeController.java
@@ -0,0 +1,22 @@
+package qword.spring.jokeapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import qword.spring.jokeapp.service.JokeService;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+        return "chucknorris";
+    }
+}
