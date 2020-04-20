22336e3704dd23295300bd7640fee08e40a941a9
Gianluigi Disconzi <gianluigi.disconzi@gmail.com>
2020-04-20 17:09:33 +0200

    Replace Java based with XML based configuration

--- src/main/java/qword/spring/jokeapp/JokeappApplication.java
+++ src/main/java/qword/spring/jokeapp/JokeappApplication.java
@@ -2,8 +2,10 @@ package qword.spring.jokeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:joke-config.xml")
 public class JokeappApplication {
 
     public static void main(String[] args) {
--- src/main/java/qword/spring/jokeapp/configuration/JokeConfiguration.java
+++ src/main/java/qword/spring/jokeapp/configuration/JokeConfiguration.java
@@ -1,13 +1,11 @@
 package qword.spring.jokeapp.configuration;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class JokeConfiguration {
 
-    @Bean
+//    @Bean
     public ChuckNorrisQuotes chuckNorrisQuotes() {
         return new ChuckNorrisQuotes();
     }
